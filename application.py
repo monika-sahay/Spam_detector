15ba771fa4f1e5b8a1c4058f3c4d4cdd44dfabf0
monika-sahay <monika_sahay@yahoo.com>
2023-05-24 20:09:23 +0000

    added api to detect spam or ham

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
-app = Flask(__name__)
+import joblib
 
+
+app = Flask(__name__)
+vectorizer=joblib.load("vectorizer.pkl")
+spamorham_model=joblib.load("spam_ham_model.pkl")
 
 @app.route('/')
 def hello_world():
@@ -9,7 +13,9 @@ def hello_world():
 @app.route('/spamorham', methods=['GET','POST'])
 def spamorham():
     message = request.args.get("message")
-    return message
+    vect_message = vectorizer.transform([message])
+    result = spamorham_model.predict(vect_message)[0]
+    return result
 
 if __name__ == '__main__':
     app.run()
